94924a4b6d7a9231a87c28dd509215b08ae4acc1
steff <drlef147@gmail.com>
2023-01-07 14:18:04 +0100

    add yaml serialization. improve print output

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use std::{fs, fmt};
+use std::{env, fs, fmt};
 use std::path::{Path, PathBuf};
+use serde::{Serialize, Deserialize};
 
-// we need a structure that holds directories and files
-// A directory can hold 0 or more directories and 0 or more files
-// a file is just a Path
+#[derive(Serialize, Deserialize)]
+struct DirectoryEntry {
+    dir_path: PathBuf,
+    dir: Directory,
+}
+
+impl DirectoryEntry {
+    fn new(dir_path: PathBuf, dir: Directory) -> Self {
+        Self {
+            dir_path,
+            dir,
+        }
+    }
+}
+
+impl fmt::Display for DirectoryEntry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut dir_str = self.dir_path.display().to_string() + "/\n";
+
+        dir_str = dir_str + &self.dir.to_string();
+        write!(f, "{}", dir_str)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 struct Directory {
     files: Vec<PathBuf>,
-    dirs: Vec<Directory>,
+    dirs: Vec<DirectoryEntry>,
 }
 
 impl Directory {
-    fn new(files: Vec<PathBuf>, dirs: Vec<Directory>) -> Self {
+    fn new(files: Vec<PathBuf>, dirs: Vec<DirectoryEntry>) -> Self {
         Self {
             files,
             dirs,
@@ -26,8 +49,7 @@ impl fmt::Display for Directory {
             files_string = files_string + file_str + "\n";
         }
         for dir in &self.dirs {
-            let dir_str = &dir.to_string();
-            files_string = files_string + dir_str;
+            files_string = files_string + &dir.to_string();
         }
         write!(f, "{}", files_string)
     }
@@ -50,17 +72,33 @@ fn crawl_dir(dir_path: &Path) -> Directory {
         .iter()
         .filter(|path| path.is_dir())
         .collect::<Vec<_>>();
-    
+
+    let file_paths = paths
+        .iter()
+        .filter(|path| path.is_file())
+        .map(|path| path.clone())
+        .collect::<Vec<_>>();
+
     let mut subdirs = Vec::new();
     for dir in subdir_paths {
-        subdirs.push(crawl_dir(dir));
+        println!("subdir: {:?}", dir);
+        subdirs.push(DirectoryEntry::new(dir.clone(), crawl_dir(dir)));
     }
 
-    let root_dir = Directory::new(paths, subdirs);
+    let root_dir = Directory::new(file_paths, subdirs);
     root_dir
 }
 
 fn main() {
-    let dir = crawl_dir(&PathBuf::from("/home/steff"));
-    println!("{}", dir);
+    let args: Vec<String> = env::args().collect();
+
+    let mut dir_path = ".";
+    if !args.is_empty() {
+        dir_path = &args[1];
+    }
+
+    let dir = crawl_dir(&PathBuf::from(dir_path));
+    //println!("{}", dir);
+    println!("{}", serde_yaml::to_string(&dir).unwrap());
 }
+
